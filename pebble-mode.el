ae7a5cd2f860dbe3c1ba5d4a4856188cd45dad61
Arne Babenhauserheide <arne_bab@web.de>
2022-10-02 10:11:59 +0200

    add dependency to Emacs 24.3

--- pebble-mode.el
+++ pebble-mode.el
@@ -8,6 +8,7 @@
 ;; Author: Florian Mounier aka paradoxxxzero
 ;; Author: Arne Babenhauserheide
 ;; Version: 0.3
+;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
