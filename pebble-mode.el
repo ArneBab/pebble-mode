bcbc76aa89196338f12a8ddfe4486edf83c19c5e
Arne Babenhauserheide <arne_bab@web.de>
2023-01-23 19:01:26 +0100

    thank Disy in commentary for the sponsorship of the development

--- pebble-mode.el
+++ pebble-mode.el
@@ -32,6 +32,8 @@
 
 ;; It follows pebbles syntax reference: https://pebbletemplates.io/wiki/guide/basic-usage/
 
+;; Implementation sponsored by my employer Disy Informationssysteme GmbH. Thank you!
+
 ;; pebble-mode is based on jinja2-mode by Florian Mounier
 
 ;;; TODO:
